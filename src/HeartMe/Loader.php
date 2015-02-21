ce89fcbfad5c7624e6aa287df2c3c8e331755665
Gamecrafter <pro_victor@ymail.com>
2015-02-20 20:27:40 -0800

    Added /dump

--- src/HeartMe/Loader.php
+++ src/HeartMe/Loader.php
@@ -21,10 +21,30 @@ class Loader extends PluginBase{
     
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
         switch($command->getName()){
-            case "disable":
+            case "date":
                 if($sender->hasPermission("heartme.command.date")){
                     if($sender instanceof Player){
-                        
+                        $player = $sender->getName;
+                        $target = $this->getServer->getPlayer($args[0]);
+                        $target->sendMessage("$player wants to date you!")
+                        $this->getServer()->broadcastMessage("There's a new couple in town: $player and $target!")
+                    }
+                    else{
+                        $sender->sendMessage(TextFormat::RED."Please run this command in-game.");
+                    }
+                }
+                else{
+                    $sender->sendMessage(TextFormat::RED."You don't have permissions to use this command.");
+                    return true;
+                }
+            break;
+            case "dump":
+                if($sender->hasPermission("heartme.command.dump")){
+                    if($sender instanceof Player){
+                        $player = $sender->getName;
+                        $target = $this->getServer->getPlayer($args[0]);
+                        $target->sendMessage("$player has dumped you!")
+                        $this->getServer()->broadcastMessage("$player dumped $target!")
                     }
                     else{
                         $sender->sendMessage(TextFormat::RED."Please run this command in-game.");
